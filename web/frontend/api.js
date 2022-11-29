eb904f67215a9024917fa2de2ebb26b14e4f4e4b
Antoine Charette <antoineocharette@gmail.com>
2022-11-29 10:11:23 -0500

    Adds a code snippet for the client instance

--- web/frontend/api.js
+++ web/frontend/api.js
@@ -1,3 +1,6 @@
 import { Client } from "@gadget-client/heroku-deployment-test";
 
-export const api = new Client();
+export const api = new Client({
+  environment:
+    process.env.NODE_ENV === "production" ? "Production" : "Development",
+});
