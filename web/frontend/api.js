f22f4167f3af649c3835de3990a26cbb708e1430
Antoine Charette <antoineocharette@gmail.com>
2023-01-17 13:52:08 -0500

    Changed the code to follow our new CLI Connection docs

--- web/frontend/api.js
+++ web/frontend/api.js
@@ -1,6 +1,3 @@
 import { Client } from "@gadget-client/heroku-deployment-test";
 
-export const api = new Client({
-  environment:
-    process.env.NODE_ENV === "production" ? "Production" : "Development",
-});
+export const api = new Client();
